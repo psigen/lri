f38855f86b6b26906498517706f111f878b61408
Joydeep Biswas <joydeep.biswas@gmail.com>
2013-02-16 15:05:33 -0500

    Discovery API: First draft

--- include/discovery.h
+++ include/discovery.h
@@ -1,3 +1,7 @@
+#include "topic_query.h"
+#include "topic_publisher.h"
+#include "topic_notice.h"
+
 #ifndef LRI_INCLUDE_DISCOVERY_H_
 #define LRI_INCLUDE_DISCOVERY_H_
 
@@ -10,6 +14,24 @@ namespace lri {
  */
 class Discovery {
   
+  /**
+   * Takes in a vector of topics (QueryTopic) that the node is interested in, 
+   * and returns a vector of publishers that are publishing to those topics.
+   */
+  Query(const std::vector<QueryTopic>& topics, 
+        std::vector<PublishTopic>* publishers);
+
+  /**
+   * Takes in a vector of topics (PublishTopic) that the node is going to 
+   * publish to, for advertising them and for replying to topic queries.
+   */
+  Provide(const std::vector<PublishTopic>& topics); 
+  
+  /**
+   * Takes in a vector of topics (PublishTopic) that the node previously
+   * was publishing to, but is no longer going to be publishing to.
+   */
+  UnProvide(const std::vector<PublishTopic>& topics); 
 };
 
 }  // namespace lri
