15a422ef9d7642741deef97b295c6116129750b4
Joydeep Biswas <joydeep.biswas@gmail.com>
2013-05-05 20:11:59 -0400

    Added tester executable to test Poco UDP sockets:

---
+++ src/test/udp_server_tester.cc
@@ -0,0 +1,16 @@
+#include "Poco/Net/SocketAddress.h"
+#include "Poco/Net/MulticastSocket.h"
+int main(int argc, char* argv[])
+{
+  Poco::Net::SocketAddress address("239.255.255.250", 1900);
+  Poco::Net::MulticastSocket socket(
+      Poco::Net::SocketAddress(
+      Poco::Net::IPAddress(), address.port()));
+  // to receive any data you must join
+  socket.joinGroup(address.host());
+  Poco::Net::SocketAddress sender;
+  char buffer[512];
+  int n = socket.receiveFrom(buffer, sizeof(buffer), sender);
+  socket.sendTo(buffer, n, sender);
+  return 0;
+}
