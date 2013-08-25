0e8339f450162ae7cc006119f399ab1102d8a090
Joydeep Biswas <joydeep.biswas@gmail.com>
2013-08-25 16:49:50 -0400

    Updated UDP discovery

--- include/discovery.h
+++ include/discovery.h
@@ -1,6 +1,6 @@
-#include "topic_query.h"
-#include "topic_publisher.h"
-#include "topic_notice.h"
+#include "topic_query.pb.h"
+#include "topic_publisher.pb.h"
+#include "topic_notice.pb.h"
 
 #ifndef LRI_INCLUDE_DISCOVERY_H_
 #define LRI_INCLUDE_DISCOVERY_H_
@@ -21,27 +21,27 @@ class Discovery {
    * Takes in a vector of topics (QueryTopic) that the node is interested in,
    * and returns a vector of publishers that are publishing to those topics.
    */
-  virtual void Query(const std::vector<QueryTopic>& topics,
-                     std::vector<PublishTopic>* publishers) = 0;
+  virtual void Query(const std::vector<lri::TopicQuery>& topics,
+                     std::vector<lri::TopicPublisher>* publishers) = 0;
 
   /**
    * Takes in a vector of topics (PublishTopic) that the node is going to
    * publish to, for advertising them and for replying to topic queries.
    */
-  virtual void Register(const std::vector<PublishTopic>& topics) = 0;
+  virtual void Register(const std::vector<lri::TopicQuery>& topics) = 0;
 
   /**
    * Takes in a vector of topics (PublishTopic) that the node previously
    * was publishing to, but is no longer going to be publishing to.
    */
-  virtual void Unregister(const std::vector<PublishTopic>& topics) = 0;
+  virtual void Unregister(const std::vector<lri::TopicQuery>& topics) = 0;
 
   /**
    * Defines a callback function that will be called when a new publisher is
    * discovered for a subscribed topic.
    */
-  typedef void (*DiscoveryCallback)(const std::vector<PublishTopic>& topics,
-      void* context);
+  typedef void (*DiscoveryCallback)(
+      const std::vector<lri::TopicQuery>& topics, void* context);
 
   virtual void SetCallback(DiscoveryCallback callback) = 0;
 };
--- src/discovery/discovery_udp.cc
+++ src/discovery/discovery_udp.cc
@@ -1,9 +1,56 @@
 #include "discovery/discovery_udp.h"
+#include "Poco/Net/SocketAddress.h"
+#include "Poco/Net/MulticastSocket.h"
+#include "Poco/Net/DatagramSocket.h"
+#include "Poco/Runnable.h"
+#include "Poco/Thread.h"
+
+using std::size_t;
+
+namespace {
+
+static const int kLriDiscpveryUdpPort = 9390;
+static const char* kLriDiscpveryUdpGroup = "225.1.0.37";
+static const Poco::Net::SocketAddress kLriDiscoverySocketAddress(
+    kLriDiscpveryUdpGroup, kLriDiscpveryUdpPort);
+
+class DiscoveryUDPFunction : public Poco::Runnable {
+ public:
+  DiscoveryUDPFunction() : Poco::Runnable() : run_(true) {}
+  virtual ~DiscoveryUDPFunction() {};
+
+ private:
+  virtual void run() {
+    // Until run_ becomes false, wait for messages to be received on UDP
+    // multicast.
+    // When new data is received, parse the proto message and update the list
+    // of topic publishers accordingly.
+  }
+
+ public:
+  void Shutdown() {
+    run_ = false;
+  }
+
+private:
+  bool run_;
+};
+
+}  // namespace
 
 namespace lri {
 
+typedef Poco::Thread Thread;
+typedef Poco::Net::DatagramSocket DatagramSocket;
+
 DiscoveryUDP::DiscoveryUDP() {
   // Start a new thread to listen on the UDP multicast.
+  discovery_thread_ = new Thread();
+  discovery_thread_->start(new DiscoveryUDPFunction);
+
+  // Prepare an instance of a multicast datagram socket interface that will be
+  // used to publish to the UDP multicast.
+  socket_ = new DatagramSocket();
 }
 
 DiscoveryUDP::~DiscoveryUDP() {
@@ -11,20 +58,25 @@ DiscoveryUDP::~DiscoveryUDP() {
 }
 
 void DiscoveryUDP::Query(
-    const std::vector<QueryTopic>& topics,
-    std::vector<PublishTopic>* publishers) {
+  const std::vector<TopicQuery>& topics,
+    std::vector<TopicPublisher>* publishers) {
   // Send out multicast QueryTopic packets for every topic.
+  for (size_t i = 0; i < topics.size(); ++i) {
+    
+  }
 
   // Wait for response from peers on the multicast address and return
   // responses.
 }
 
-void DiscoveryUDP::Register(const std::vector<PublishTopic>& topics) {
+void DiscoveryUDP::Register(const std::vector<TopicQuery>& topics) {
   // Save the list of topics.
+  publishing_topics_.insert(publishing_topics_.end(), topics.begin(),
+                            topics.end());
   // Announce the topics.
 }
 
-void DiscoveryUDP::Unregister(const std::vector<PublishTopic>& topics) {
+void DiscoveryUDP::Unregister(const std::vector<TopicQuery>& topics) {
   // Remove the topics from the saved list.
   // Send unregister notice to the multicast address.
 }
