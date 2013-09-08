a49ce1fe70e1b2e39b1095a505421b91c91af7c0
Pras Velagapudi <notpras@gmail.com>
2013-09-08 17:28:41 -0400

    Reworked proto files for discovery to match new protocol with symmetric pub/sub notifications.

--- src/shared/proto_helpers.h
+++ src/shared/proto_helpers.h
@@ -4,9 +4,10 @@
 #ifndef SRC_SHARED_PROTO_HELPRS_H
 #define SRC_SHARED_PROTO_HELPRS_H
 
-
+// Serializes a Protobuf message with a 32-bit length prefix to a provided
+// buffer.
 template <typename T>
-Serialize(const T& message, std::vector<uint8_t>* buffer) {
+SerializeWithLength(const T& message, std::vector<uint8_t>* buffer) {
 }
 
 #endif  // SRC_SHARED_PROTO_HELPRS_H
